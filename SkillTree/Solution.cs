bb45593438b34b7c019bcf26835bc3e8124d0ae2
DongSoon Kim <sekom89@gmail.com>
2020-01-26 14:15:16 +0900

    문제풀이중

---
+++ SkillTree/Solution.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for https://programmers.co.kr/learn/courses/30/lessons/49993
+/// </summary>
+public class Solution
+{
+    public int solution(string skill, string[] skill_trees)
+    {
+        int answer = 0;
+        int skill_trees_max = 20;
+
+        char[] condition = SkillToCondition(skill);
+
+        for(int i = 0; i < skill_trees.Length; ++i)
+        {
+            if(Checker(condition, skill_trees[i]))
+            {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public char[] SkillToCondition(string skill)
+    {
+        if (skill.Empty == true)
+            return null;
+
+        char[] condition = new char[skill.Length];
+
+        for (int i= 0; i< skill.Length; i++)
+        {
+            condition[i] = skill[i];
+        }
+
+        return condition;
+    }
+
+    public bool Checker(char[] condition, string skill)
+    {
+        if (condition.Empty == true || skill.Empty == true)
+            return false;
+
+        int conditionIndex = -1;
+        int currentIndex = -1;
+        for(int i = 0; i < skill.Length; i++)
+        {
+            currentIndex = GetCoditionIndex(skill[i]);
+            if( currentIndex > conditionIndex)
+            {
+
+            }
+        }
+
+        return false;
+    }
+
+    public int GetCoditionIndex(char[] condition, char c)
+    {
+        for(int i = 0; i < condition.Length; i++)
+        {
+            if(condition[i] == c)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    
+}
